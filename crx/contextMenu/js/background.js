ff152fa21ae2fd2245113257d8fc6ef979750667
zcc <zcccg126@126.com>
2019-10-15 19:22:35 +0800

    add

--- crx/contextMenu/js/background.js
+++ crx/contextMenu/js/background.js
@@ -2,8 +2,16 @@ chrome.contextMenus.create({
   'type':'normal',
   'title':'使用Google翻译……',
   'contexts':['selection'],
-  'id':'cn',
+  'id':'google',
   'onclick':translate
+});
+
+chrome.contextMenus.create({
+  'type':'normal',
+  'title':'使用百度搜索……',
+  'contexts':['selection'],
+  'id':'cn',
+  'onclick':search
 });
 
 chrome.contextMenus.create({
@@ -68,9 +76,15 @@ function translate(info, tab){
   var url = 'http://translate.google.com.hk/#auto/zh-CN/'+info.selectionText ;
   window.open(url, '_blank');
 }
-
+function search(key){
+  var url = 'https://www.baidu.com/s?wd='+key.selectionText ;
+  window.open(url, '_blank');
+}
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
   chrome.contextMenus.update('cn',{
-      'title':'使用Google翻译“'+message+'”'
+      'title':'使用百度查询“'+message+'”'
   });
+  chrome.contextMenus.update('google',{
+    'title':'使用Google翻译“'+message+'”'
+});
 });
